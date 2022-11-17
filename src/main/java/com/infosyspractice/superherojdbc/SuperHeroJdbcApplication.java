24f278b7686bc272c6938e1dc580377dc8c84870
formula38 <louismconley@gmail.com>
2022-11-17 10:33:05 -0800

    Initial Commit

---
+++ src/main/java/com/infosyspractice/superherojdbc/SuperHeroJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.infosyspractice.superherojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperHeroJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infosyspractice/superherojdbc/SuperHeroJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosyspractice.superherojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
